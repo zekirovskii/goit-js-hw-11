a012e7515c5c755d663fad3b111360af52c07633
yusuf sengoz <sengozyusuf91@gmail.com>
2025-05-16 21:24:53 +0300

    fixed

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,67 +1,17 @@
-import { renderImages, renderNoResults, clearGallery } from "./render-functions.js";
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import axios from 'axios';
 
-const baseUrl = `https://pixabay.com/api/`;
-const apiKey = `50314234-ba813915cbcdccb3e2319b70f`;
+const API_KEY = '50314234-ba813915cbcdccb3e2319b70f';
+const BASE_URL = 'https://pixabay.com/api/';
 
-const searchForm = document.getElementById("searchForm");
-const gallery = document.querySelector(".gallery");
+export function fetchImages(query) {
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
 
-
-const loader = document.getElementById('loader');
-function showLoader() {
-  loader.classList.remove('hidden');
-}
-function hideLoader() {
-  loader.classList.add('hidden');
+  return axios.get(BASE_URL, { params });
 }
 
-
-searchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  clearGallery();
-
-  const searchWord = document.getElementById("searchInput").value.trim();
-
-    showLoader();
-    
-    if (!searchWord) {
-        hideLoader();
-    
-    return;
-  }
-
-  const params = new URLSearchParams({
-    key: apiKey,
-    q: searchWord,
-    image_type: "photo",
-    orientation: "horizontal",
-    safesearch: "true",
-  });
-
-  const url = `${baseUrl}?${params.toString()}`;
-   
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      setTimeout(() => {
-        if (data.hits.length === 0) {
-          renderNoResults();
-        } else {
-          renderImages(data.hits);
-        }
-        hideLoader();
-      }, 1000);
-    })
-    .catch(error => {
-      iziToast.error({
-        title: "Hata",
-        message: "API isteğinde hata oluştu.",
-        position: "topRight",
-      });
-      console.error("Fetch error:", error);
-      hideLoader();
-    });
-});
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,58 +1,31 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import iziToast from "izitoast";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const gallery = document.querySelector('.gallery');
 let lightbox = null;
 
-export function renderImages(images) {
-  const gallery = document.querySelector(".gallery");
+export function renderGallery(images) {
   const markup = images
     .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => `
-      <a class="gallery-item" href="${largeImageURL}" target="_blank" rel="noopener noreferrer">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      image => `
+      <a href="${image.largeImageURL}" class="gallery-item">
+        <img src="${image.webformatURL}" alt="${image.tags}" />
         <div class="info">
-          <p><b>Likes:</b> ${likes}</p>
-          <p><b>Views:</b> ${views}</p>
-          <p><b>Comments:</b> ${comments}</p>
-          <p><b>Downloads:</b> ${downloads}</p>
+          <p><b>Likes:</b> ${image.likes}</p>
+          <p><b>Views:</b> ${image.views}</p>
+          <p><b>Comments:</b> ${image.comments}</p>
+          <p><b>Downloads:</b> ${image.downloads}</p>
         </div>
       </a>
     `
     )
-    .join("");
+    .join('');
 
-  gallery.innerHTML = markup;
+  gallery.insertAdjacentHTML('beforeend', markup);
 
   if (!lightbox) {
-    lightbox = new SimpleLightbox(".gallery a", {
-      captionsData: "alt",
-      captionDelay: 250,
-    });
+    lightbox = new SimpleLightbox('.gallery a');
   } else {
     lightbox.refresh();
   }
-}
-
-export function clearGallery() {
-  const gallery = document.querySelector(".gallery");
-  gallery.innerHTML = "";
-}
-
-export function renderNoResults() {
-  clearGallery();
-
-  iziToast.error({
-    message: "Sorry, there are no images matching your search query. Please try again!",
-      position: "topRight",
-    
-  });
 }
